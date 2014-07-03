9a20b2d3ff233cafad52200d20414fbaa4fa6671
Ali Hajimirza <a92hm@yahoo.com>
2014-07-02 18:16:31 -0700

    express 4

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function(req, res) {
   res.status(404).render('404', { title: '404' });
 });
 
-if (process.env.NODE_ENV == 'production') {
+if (app.get('env') === 'development') {
   module.export = app;
 } else {
   app.set('port', process.env.PORT || 3000);
