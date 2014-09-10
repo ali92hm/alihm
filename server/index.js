07ea59ca8e90ea477f6419259dfe8527adffbd1a
Ali Hajimirza <a92hm@yahoo.com>
2014-09-10 17:16:49 -0500

    use mongo in production

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ var express = require('express')
   , logger = require('morgan')
   , bodyParser = require('body-parser')
   , favicon = require('serve-favicon')
-  , mongoose = require('mongoose')
-  , controller = require('./message.controller')
   , cookieParser = require('cookie-parser');
 
 var app = express();
@@ -12,19 +10,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-mongoose.connect('mongodb://localhost/alihm');
+
 
 //Router
 app.use(express.static(path.join(__dirname, '..' ,'public')));
 app.use(express.static(path.join(__dirname, '..' ,'bower_components')));
 app.use(favicon(path.join(__dirname, '..','public','images','favicon.png')));
-app.get('/view/message', controller.index);
-app.post('/message', controller.create);
-app.get('*', function(req, res, next){
-  res.status(404).sendFile(path.join(__dirname, '..' ,'public', '404.html'));
-});
+
 
 if (app.get('env') === 'production') {
+  var mongoose = require('mongoose');
+  var controller = require('./message.controller');
+  mongoose.connect('mongodb://localhost/alihm');
+  app.get('/view/message', controller.index);
+  app.post('/message', controller.create);
   module.exports = app;
 } else {
   app.set('port', process.env.PORT || 3000);
@@ -32,3 +31,7 @@ if (app.get('env') === 'production') {
     console.log('Express server listening on port ' + server.address().port);
   });
 }
+
+app.get('*', function(req, res, next){
+  res.status(404).sendFile(path.join(__dirname, '..' ,'public', '404.html'));
+});
