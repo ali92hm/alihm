a1ebdfa84451495097d7873f1e53dd10b3d989c4
Ali Hajimirza <a92hm@yahoo.com>
2015-01-07 18:09:36 -0600

    close function on notification

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,60 +1,60 @@
 /*
-	Miniport by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Miniport by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 'use strict';
 (function($) {
 
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			'global':	{ range: '*', href: '/styles/style.css' },
-			'desktop':	{ range: '737-', href: '/styles/style-desktop.css', containers: 1200, grid: { gutters: 25 } },
-			'1000px':	{ range: '737-1200', href: '/styles/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewport: { width: 1080 } },
-			'mobile':	{ range: '-736', href: '/styles/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 15 }, viewport: { scalable: false } }
-		}
-	});
+    skel.init({
+        reset: 'full',
+        breakpoints: {
+            'global':   { range: '*', href: '/styles/style.css' },
+            'desktop':  { range: '737-', href: '/styles/style-desktop.css', containers: 1200, grid: { gutters: 25 } },
+            '1000px':   { range: '737-1200', href: '/styles/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewport: { width: 1080 } },
+            'mobile':   { range: '-736', href: '/styles/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 15 }, viewport: { scalable: false } }
+        }
+    });
 
-	$(function() {
+    $(function() {
 
-		var	$window = $(window),
-			$body = $('body');
-			
-		// Disable animations/transitions until the page has loaded.
-		$body.addClass('is-loading');
-		
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
-
-		$('.close').click(function(){
-            $('.notification.error').slideUp(300);
-            $('.notification.success').slideUp(300);
-            return false;
+        var $window = $(window),
+            $body = $('body');
+            
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
+        
+        $window.on('load', function() {
+            $body.removeClass('is-loading');
         });
-		// CSS polyfills (IE<9).
-		if (skel.vars.IEVersion < 9){
-			$(':last-child').addClass('last-child');
-		}
 
-		// Scrolly.
-		$window.load(function() {
+        // CSS polyfills (IE<9).
+        if (skel.vars.IEVersion < 9){
+            $(':last-child').addClass('last-child');
+        }
 
-			var x = parseInt($('.wrapper').first().css('padding-top')) - 15;
-			$('#nav a, .scrolly').scrolly(1000, x);
+        // Scrolly.
+        $window.load(function() {
 
-		});
-		
-	});
+            var x = parseInt($('.wrapper').first().css('padding-top')) - 15;
+            $('#nav a, .scrolly').scrolly(1000, x);
+
+        });
+        
+    });
 
 })(jQuery);
 
 $(function(){
+    $('.close').click(function(){
+        $('.notification.error').slideUp(300);
+        $('.notification.success').slideUp(300);
+        return false;
+    });
     // Submitting forms
     $('#contact_us').on('submit', function(){
-    	console.log('hey');
+        console.log('hey');
         var error = $('.notification.error');
         var success = $('.notification.success');
         success.slideUp(300);
