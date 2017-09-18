ff2dde097b7f33471f93b0a6bae84d37b53411d9
ali92hm <ali92hm@gmail.com>
2017-09-17 22:49:37 -0700

    added redirect urls

--- server/config/config.js
+++ server/config/config.js
@@ -12,5 +12,14 @@ module.exports = {
     server: process.env.LETS_ENCRYPT_SERVER || 'staging',
     webrootPath: process.env.LETS_ENCRYPT_CERT_PATH || '/tmp/acme-challenges',
     email: process.env.LETS_ENCRYPT_EMAIL
+  },
+  redirects: {
+    'iss': 'http://www.thequestinstitute.com/ISS/',
+    'webjet': 'https://channel9.msdn.com/Events/Build/2017/T6979',
+    'slotcar': 'https://www.youtube.com/watch?v=PDZbvw0xE-k',
+    'real': 'https://github.com/ou-real',
+    'igert': '',
+    'parallel': '',
+    'resume': ''
   }
 }
