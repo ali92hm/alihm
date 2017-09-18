e8c6b004934136498de3f53cc9fb256c369086e0
ali92hm <ali92hm@gmail.com>
2017-09-17 19:34:33 -0700

    trimed dep

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,34 @@
 // Generated on 2014-12-30 using generator-yeogurt 0.12.3
-'use strict';
 
 // Folder paths for:
 // - dev (location for development source files)
 // - server (location for files created when running 'grunt serve')
 // - dist (location for files created when running 'grunt' or 'grunt build')
 var config = {
-    client: 'client',
-    staticServer: 'client/.serve',
-    dist: 'dist',
-    server: 'server'
-};
+  client: 'client',
+  staticServer: 'client/.serve',
+  dist: 'dist',
+  server: 'server'
+}
 
-module.exports = function(grunt) {
-
+module.exports = (grunt) => {
     // setup configuration object
-    grunt.config.set('yeogurt', config);
+  grunt.config.set('yeogurt', config)
 
     // show elapsed time at the end
-    require('time-grunt')(grunt);
+  require('time-grunt')(grunt)
 
     // Load all grunt tasks with JIT (Makes task loading super fast!)
-    require('jit-grunt')(grunt, {
+  require('jit-grunt')(grunt, {
         // translate useminPrepare to use the 'grunt-usemin' plugin
-        useminPrepare: 'grunt-usemin',
+    useminPrepare: 'grunt-usemin',
         // translate swig to use the 'grunt-wobble-swig' plugin
-        swig: 'grunt-swig-templates'
-    });
+    swig: 'grunt-swig-templates'
+  })
 
     // Load the include-all library in order to require all of our grunt
     // configurations and task registrations dynamically.
-    var includeAll = require('include-all');
+  var includeAll = require('include-all')
 
     /**
      * Loads Grunt configuration modules from the specified
@@ -38,46 +36,43 @@ module.exports = function(grunt) {
      * that, when run, should either load/configure or register
      * a Grunt task.
      */
-    function loadTasks(relPath) {
-        return includeAll({
-            dirname: require('path').resolve(__dirname, relPath),
-            filter: /(.+)\.js$/
-        }) || {};
-    }
+  function loadTasks (relPath) {
+    return includeAll({
+      dirname: require('path').resolve(__dirname, relPath),
+      filter: /(.+)\.js$/
+    }) || {}
+  }
 
     /**
      * Invokes the function from a Grunt configuration module with
      * a single argument - the `grunt` object.
      */
-    function invokeConfigFn(tasks) {
-        for (var taskName in tasks) {
-            if (tasks.hasOwnProperty(taskName)) {
-                tasks[taskName](grunt);
-            }
-        }
+  function invokeConfigFn (tasks) {
+    for (var taskName in tasks) {
+      if (tasks.hasOwnProperty(taskName)) {
+        tasks[taskName](grunt)
+      }
     }
+  }
 
     // Load task functions
-    var utilConfig = loadTasks('./grunt/config/util');
-    var compileConfig = loadTasks('./grunt/config/compile');
-    var optimizeConfig = loadTasks('./grunt/config/optimize');
-    var serverConfig = loadTasks('./grunt/config/server');
-    var registerDefinitions = loadTasks('./grunt/tasks');
+  var utilConfig = loadTasks('./grunt/config/util')
+  var optimizeConfig = loadTasks('./grunt/config/optimize')
+  var serverConfig = loadTasks('./grunt/config/server')
+  var registerDefinitions = loadTasks('./grunt/tasks')
 
     // (ensure that a default task exists)
-    if (!registerDefinitions.
-        default) {
-        registerDefinitions.
-        default = function(grunt) {
-            grunt.registerTask('default', []);
-        };
-    }
+  if (!registerDefinitions
+        .default) {
+    registerDefinitions
+        .default = (grunt) => {
+          grunt.registerTask('default', [])
+        }
+  }
 
     // Run task functions to configure Grunt.
-    invokeConfigFn(utilConfig);
-    invokeConfigFn(compileConfig);
-    invokeConfigFn(optimizeConfig);
-    invokeConfigFn(serverConfig);
-    invokeConfigFn(registerDefinitions);
-
-};
+  invokeConfigFn(utilConfig)
+  invokeConfigFn(optimizeConfig)
+  invokeConfigFn(serverConfig)
+  invokeConfigFn(registerDefinitions)
+}
