a6cb16e2feb07f2f0580cffc6c3b1efa81b90982
ali92hm <ali92hm@gmail.com>
2017-09-17 22:04:06 -0700

    refactor and renamed

--- server/app.js
+++ server/app.js
@@ -5,19 +5,19 @@ const app = express()
 require('./config/express')(app, config)
 
 if (config.isProduction) {
-  const ssl = require('./config/ssl')(config)
+  const letsEncrypt = require('./config/lets-encrypt')(config)
   // handles acme-challenge and redirects to https
-  require('http').createServer(ssl.middleware(require('redirect-https')())).listen(config.port, () => {
+  require('http').createServer(letsEncrypt.middleware(require('redirect-https')())).listen(config.port, () => {
     console.log('Listening for ACME http-01 challenges on', config.port)
   })
 
   // handles app
-  require('https').createServer(ssl.httpsOptions, ssl.middleware(app)).listen(config.httpsPort, () => {
+  require('https').createServer(letsEncrypt.httpsOptions, letsEncrypt.middleware(app)).listen(config.httpsPort, () => {
     console.log('app listening on port', config.httpsPort)
   })
 } else {
   app.listen(config.port, () => {
-    console.log('Express server listening on port ' + config.port)
+    console.log('Express server listening on port', config.port)
   })
 }
 
