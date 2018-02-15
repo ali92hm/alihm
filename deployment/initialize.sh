2463f28a03f0103986b21e258f95d13cd5538f36
ali92hm <ali92hm@gmail.com>
2018-02-15 14:31:24 -0800

    added helm chart

---
+++ deployment/initialize.sh
@@ -0,0 +1,21 @@
+# Initializing helm on a cluster
+helm init
+
+# Installing kube-lego for Let's encrypt
+kubectl create namespace cert-management
+helm install --name cert-tls \
+  --namespace cert-management \
+  --set config.LEGO_EMAIL=ali@alihm.net \
+  --set config.LEGO_URL=https://acme-v01.api.letsencrypt.org/directory \
+    stable/kube-lego
+
+# Install ingress controller
+kubectl create namespace public
+helm install --name ingress \
+  --namespace public \
+  stable/nginx-ingress
+
+kubectl create namespace alihm
+helm install --name website \
+  --namespace alihm \
+  alihm
