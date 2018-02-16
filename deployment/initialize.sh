958904dd96489f05c6c2ae850aa6ab445f3310f0
ali92hm <ali92hm@gmail.com>
2018-02-15 18:28:32 -0800

    added dev environment

--- deployment/initialize.sh
+++ deployment/initialize.sh
@@ -16,6 +16,14 @@ helm install --name ingress \
   stable/nginx-ingress
 
 kubectl create namespace alihm
-helm install --name website \
+helm install --name alihm-prod \
   --namespace alihm \
   alihm
+
+helm install --name alihm-staging \
+    --namespace alihm \
+    --set image.tag=latest \
+    --set image.replicaCount=1 \
+    --set ingress.host=staging.alihm.net \
+    --set ingress.redirectWWW=false \
+    alihm
